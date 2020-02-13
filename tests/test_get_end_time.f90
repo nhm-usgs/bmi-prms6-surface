447540e9d3d07694741256ecc66ce998e4495bf5
Mark Piper <mark.piper@colorado.edu>
2020-02-13 11:35:14 -0700

    Add tests for time functions
    Note: the default end time > default time step.

--- tests/test_get_end_time.f90
+++ tests/test_get_end_time.f90
@@ -1,21 +1,21 @@
 program test_get_end_time
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
   use fixtures, only: config_file, status
 
   implicit none
 
-  double precision, parameter :: expected_time = 20.d0
+  double precision, parameter :: expected_time = 13180.d0
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   double precision :: end_time
 
   status = m%initialize(config_file)
   status = m%get_end_time(end_time)
   status = m%finalize()
 
-  if (end_time.ne.expected_time) then
+  if (end_time /= expected_time) then
      stop BMI_FAILURE
   end if
 end program test_get_end_time
--- tests/test_get_time_step.f90
+++ tests/test_get_time_step.f90
@@ -1,21 +1,21 @@
 program test_get_time_step
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
   use fixtures, only: config_file, status
 
   implicit none
 
-  double precision, parameter :: expected_time_step = 1.d0
+  double precision, parameter :: expected_time_step = 86400.d0
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   double precision :: time_step
 
   status = m%initialize(config_file)
   status = m%get_time_step(time_step)
   status = m%finalize()
 
-  if (time_step.ne.expected_time_step) then
+  if (time_step /= expected_time_step) then
      stop BMI_FAILURE
   end if
 end program test_get_time_step
