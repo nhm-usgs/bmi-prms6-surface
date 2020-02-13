1f5eba90f3fb704c395550bfdf3e76b8339a3189
Mark Piper <mark.piper@colorado.edu>
2020-02-13 15:38:30 -0700

    Add tests for variable information functions
    The tests for *get_var_itemsize* and *get_var_nbytes* are currently
    failing, pending #12.

--- tests/test_get_var_grid.f90
+++ tests/test_get_var_grid.f90
@@ -1,22 +1,22 @@
 program test_get_var_grid
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
-  use fixtures, only: config_file, status
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
+  use fixtures, only: var_name, config_file, status
 
   implicit none
 
-  character (len=*), parameter :: var_name = "plate_surface__temperature"
   integer, parameter :: expected_id = 0
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   integer :: grid_id
 
   status = m%initialize(config_file)
   status = m%get_var_grid(var_name, grid_id)
   status = m%finalize()
 
-  if (grid_id.ne.expected_id) then
+  if (grid_id /= expected_id) then
+     write(*,*) grid_id
      stop BMI_FAILURE
   end if
 end program test_get_var_grid
--- tests/test_get_var_units.f90
+++ tests/test_get_var_units.f90
@@ -1,22 +1,22 @@
 program test_get_var_units
 
-  use bmif_1_2, only: BMI_FAILURE, BMI_MAX_UNITS_NAME
-  use bmiheatf
-  use fixtures, only: config_file, status
+  use bmif_2_0, only: BMI_FAILURE, BMI_MAX_UNITS_NAME
+  use bmiprmssurface
+  use fixtures, only: var_name, config_file, status
 
   implicit none
 
-  character (len=*), parameter :: var_name = "plate_surface__temperature"
-  character (len=*), parameter :: expected_units = "K"
+  character (len=*), parameter :: expected_units = "in"
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   character (len=BMI_MAX_UNITS_NAME) :: var_units
 
   status = m%initialize(config_file)
   status = m%get_var_units(var_name, var_units)
   status = m%finalize()
 
-  if (var_units.ne.expected_units) then
+  if (var_units /= expected_units) then
+     write(*,*) var_units
      stop BMI_FAILURE
   end if
 end program test_get_var_units
