066c85abfa73d0cba6a8c5b663f22ab12bff544f
Mark Piper <mark.piper@colorado.edu>
2020-02-13 11:20:10 -0700

    Add tests for model control functions

--- tests/test_update_until.f90
+++ tests/test_update_until.f90
@@ -1,14 +1,14 @@
 program test_update_until
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
   use fixtures, only: config_file, status
 
   implicit none
 
   double precision, parameter :: expected_time = 10.d0
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   double precision :: time
 
   status = m%initialize(config_file)
@@ -16,7 +16,7 @@ program test_update_until
   status = m%get_current_time(time)
   status = m%finalize()
 
-  if (time.ne.expected_time) then
+  if (time /= expected_time) then
      stop BMI_FAILURE
   end if
 end program test_update_until
