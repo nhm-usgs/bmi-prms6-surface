d906a1942ac109ba529d749d36bdec2876dc867c
Mark Piper <mark.piper@colorado.edu>
2020-02-18 11:32:30 -0700

    Add tests for grid shape, spacing, and origin

--- tests/test_get_grid_origin.f90
+++ tests/test_get_grid_origin.f90
@@ -1,17 +1,17 @@
 program test_get_grid_origin
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
   use fixtures, only: config_file, status
 
   implicit none
 
   integer, parameter :: grid_id = 0
-  integer, parameter :: rank = 2
+  integer, parameter :: rank = 1
   double precision, dimension(rank), parameter :: &
-       expected_origin = [0.0, 0.0]
+       expected_origin = [-1.d0]
 
-  type (bmi_heat) :: m
+  type (bmi_prms_surface) :: m
   double precision, dimension(2) :: grid_origin
   integer :: i
 
@@ -21,6 +21,7 @@ program test_get_grid_origin
 
   do i = 1, rank
      if (grid_origin(i).ne.expected_origin(i)) then
+        write(*,*) grid_origin
         stop BMI_FAILURE
      end if
   end do
--- tests/test_get_grid_shape.f90
+++ tests/test_get_grid_shape.f90
@@ -1,17 +1,17 @@
 program test_get_grid_shape
 
-  use bmif_1_2, only: BMI_FAILURE
-  use bmiheatf
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssurface
   use fixtures, only: config_file, status
 
   implicit none
 
   integer, parameter :: grid_id = 0
-  integer, parameter :: rank = 2
-  integer, dimension(rank), parameter :: expected_shape = [20, 10]
+  integer, parameter :: rank = 1
+  integer, dimension(rank), parameter :: expected_shape = [-1]
 
-  type (bmi_heat) :: m
-  integer, dimension(2) :: grid_shape
+  type (bmi_prms_surface) :: m
+  integer, dimension(rank) :: grid_shape
   integer :: i
 
   status = m%initialize(config_file)
@@ -19,7 +19,8 @@ program test_get_grid_shape
   status = m%finalize()
 
   do i = 1, rank
-     if (grid_shape(i).ne.expected_shape(i)) then
+     if (grid_shape(i) /= expected_shape(i)) then
+        write(*,*) grid_shape
         stop BMI_FAILURE
      end if
   end do
